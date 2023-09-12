f90680fc28dcfe67f9212ef9b64b86b3727cade6
DevopGirl18 <igwe18backdevopgirl@gmail.com>
2023-09-12 14:38:54 -0700

    Please check correctly

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,54 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_times_table - prints the n times table,starting
- * with 0
- * @n: number of the times table
- */
+  * print_times_table- prnits the n table
+  *@n: the times table to be printed
+  *
+  * Return: void
+  */
 void print_times_table(int n)
 {
-	int i, j, k;
+int product;
+int x;
+int y;
 
-	if (n >= 0 && n <= 15)
+if (n >= 0 && n <= 15)
+{
+	for (x = 0; x <= n; x++)
 	{
-		for (i = 0; i <= n; i++)
+		for (y = 0; y <= n; y++)
 		{
-			for (j = 0; j <= n; j++)
-			{
-			k = j * i;
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			} else if (k < 10 && j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			} else if (k >= 10 && k < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			} else if (k >= 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 100) + '0');
-				_putchar(((k / 10) % 10) + '0');
-				_putchar((k % 10) + '0');
-			}
+		product = x * y;
+		if (y == 0)
+		{
+			_putchar('0');
 		}
-			_putchar('\n');
+		else if (y != 0 && product < 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(product + '0');
 		}
-	}
+		else if (product < 100)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(product / 10 + '0');
+			_putchar(product % 10 + '0');
+		}
+		else if (product > 99)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(((product / 10)) / 10 + '0');
+			_putchar((product / 10) % 10 + '0');
+			_putchar(product % 10 + '0');
+		}
+		}
+		_putchar('\n');
+}
+}
 }

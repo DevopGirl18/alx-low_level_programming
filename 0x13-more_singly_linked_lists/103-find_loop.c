560db99c8718b7a3911d812285344d4135739165
Igweneme <141414680+DevopGirl18@users.noreply.github.com>
2023-10-24 13:25:10 +0100

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,20 +1,39 @@
 #include "lists.h"
 /**
- * find_listint_loop - finds the loop contained in a linked list
+ * find_listint_loop - finds the loop contained
+ *                     in a linked list
  * @head : pointer to the head of the linked list
- * Return: pointer the node where the loop starts or NULL - if there is no loop
- * */
+ * Return: pointer the node where the loop starts,
+ *         or NULL - if there is no loop
+ */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *node1, *node2;
-	while (node1 != NULL && node2 != NULL && node2->next != NULL)
-	{
-		node1 = node1->next;
-		node2 = node2->next->next;
-	  if (node1 == node2)
-	  {
-		  return (node1);
-	  }
-	}
-	return (NULL);
+listint_t *node1, *node2;
+
+if (head == NULL || head->next == NULL)
+return (NULL);
+
+node1 = head->next;
+node2 = (head->next)->next;
+
+while (node2)
+{
+if (node1 == node2)
+{
+node1 = head;
+
+while (node1 != node2)
+{
+node1 = node1->next;
+node2 = node2->next;
+}
+
+return (node1);
+}
+
+node1 = node1->next;
+node2 = (node2->next)->next;
+}
+
+return (NULL);
 }
